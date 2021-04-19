c47bcf56599e29916e85cf0f80f9417613c56fa9
RoaaMustafa <labushanab14@gmail.com>
2021-04-19 10:58:50 +0300

    updates

--- js/app.js
+++ js/app.js
@@ -79,11 +79,11 @@ function handleClicking(event){
       Catalog.allImages[secondIndex].votes++;
     }else if(event.target.id ==='img-three'){
       Catalog.allImages[thirdIndex].votes++;
-    // }
-    // else {
-      // alert('you should click on an image');
-      // counts--;
     }
+    // else {
+    //   alert('you should click on an image');
+    //   counts--;
+    // }
     renderThreeImages();
     console.log(Catalog.allImages);
   }else {
